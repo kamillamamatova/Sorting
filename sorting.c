53bddc571cdfd56223f105e27cae4fd932de7e2a
kamillamamatova <kamillamamatova.km@gmail.com>
2025-01-24 01:14:30 -0500

    Update sorting.c

--- sorting.c
+++ sorting.c
@@ -1,11 +1,19 @@
-// Sorting
+// Bubble Sorting
 
 #include <stdio.h>
 
 void sort(int array[], int size){
+    // Nested loop
     for(int i = 0; i < size - 1; i++){
-        for(int j = 0; j < size - 1; j++){
-            
+        for(int j = 0; j < size - i - 1; j++){
+            // Compare current number in the array with the next letter in the array
+            if(array[j] > array[j + 1]){
+                // If the current number is larger than the next, swap
+                // So that the smaller number is ahead in the array
+                int temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
         }
     }
 }
@@ -15,4 +23,11 @@ int main(){
     int size = sizeof(array)/sizeof(array[0]);  
 
     sort(array, size);
+
+    for(int i = 0; i < size; i++){
+        printf("%d " array[i]);
+    }
+
+    // Exit program
+    return 0;
 }
