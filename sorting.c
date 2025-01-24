9d726f471cef572ed611a45187250d3b191b43ad
kamillamamatova <kamillamamatova.km@gmail.com>
2025-01-24 01:07:39 -0500

    Create sorting.c

---
+++ sorting.c
@@ -0,0 +1,18 @@
+// Sorting
+
+#include <stdio.h>
+
+void sort(int array[], int size){
+    for(int i = 0; i < size - 1; i++){
+        for(int j = 0; j < size - 1; j++){
+            
+        }
+    }
+}
+
+int main(){
+    int array[] = {9, 4, 5, 2, 6, 3, 6, 7, 9};
+    int size = sizeof(array)/sizeof(array[0]);  
+
+    sort(array, size);
+}
